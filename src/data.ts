4b3ac3adffcc09e2f008227257060b2532bcafc5
Yasir <yasiroyebo@gmail.com>
2026-03-03 10:03:37 +0100

    fix: update exam details for GST course and correct venue information

--- src/data.ts
+++ src/data.ts
@@ -8,69 +8,61 @@ export interface Exam {
 }
 
 export const EXAMS: Exam[] = [
-  { 
-    id: 1, 
-    course: "GST", 
-    date: "Tuesday, March 3",
+  {
+    id: 2,
+    course: "BIO 101",
+    date: "Monday, March 9",
+    time: "9am - 3pm",
+    timestamp: "2026-03-09T09:00:00",
+    venue: "DLI BLK 1 & BLK 2"
+  },
+  {
+    id: 3,
+    course: "PHY 103",
+    date: "Wednesday, March 11",
     time: "9am - 12pm",
-    timestamp: "2026-03-03T09:00:00",
-    venue: "DLI, BK2, GFLR(CBT)" 
+    timestamp: "2026-03-11T09:00:00",
+    venue: "DLI BLK 1 & BLK 2"
   },
-  { 
-    id: 2, 
-    course: "BIO 101", 
-    date: "Monday, March 9", 
-    time: "9am - 3pm", 
-    timestamp: "2026-03-09T09:00:00", 
-    venue: "DLI, BK2, GFLR(CBT)" 
+  {
+    id: 5,
+    course: "PHY 101",
+    date: "Monday, March 16",
+    time: "9am - 3pm",
+    timestamp: "2026-03-16T09:00:00",
+    venue: "DLI BLK 1 & BLK 2"
   },
-  { 
-    id: 4, 
-    course: "ZOO 101", 
-    date: "Tuesday, March 10", 
-    time: "12pm - 3pm", 
-    timestamp: "2026-03-10T12:00:00", 
-    venue: "DLI, BK2, GFLR(CBT)" 
+  {
+    id: 6,
+    course: "COS 101/FSC 113",
+    date: "Tuesday, March 17",
+    time: "9am - 3pm",
+    timestamp: "2026-03-17T09:00:00",
+    venue: "DLI BLK 1 & BLK 2"
   },
-  { 
-    id: 3, 
-    course: "PHY 103", 
-    date: "Wednesday, March 11", 
-    time: "9am - 12pm", 
-    timestamp: "2026-03-11T09:00:00", 
-    venue: "DLI, BK2, GFLR(CBT)" 
+  {
+    id: 8,
+    course: "MTH 101",
+    date: "Tuesday, March 24",
+    time: "9am - 3pm",
+    timestamp: "2026-03-24T09:00:00",
+    venue: "DLI BLK 1 & BLK 2"
   },
-  { 
-    id: 5, 
-    course: "PHY 101", 
-    date: "Monday, March 16", 
-    time: "9am - 3pm", 
-    timestamp: "2026-03-16T09:00:00", 
-    venue: "DLI, BK2, GFLR(CBT)" 
+  {
+    id: 9,
+    course: "CHM 101",
+    date: "Wednesday, March 25",
+    time: "9am - 3pm",
+    timestamp: "2026-03-25T09:00:00",
+    venue: "DLI BLK 1 & BLK 2"
   },
-  { 
-    id: 6, 
-    course: "COS 101", 
-    date: "Tuesday, March 17", 
-    time: "9am - 3pm", 
-    timestamp: "2026-03-17T09:00:00", 
-    venue: "DLI, BK2, GFLR(CBT)" 
-  },
-  { 
-    id: 8, 
-    course: "MTH 101", 
-    date: "Tuesday, March 24", 
-    time: "9am - 3pm", 
-    timestamp: "2026-03-24T09:00:00", 
-    venue: "DLI, BK2, GFLR(CBT)" 
-  },
-  { 
-    id: 9, 
-    course: "CHM 101", 
-    date: "Wednesday, March 25", 
-    time: "9am - 3pm", 
-    timestamp: "2026-03-25T09:00:00", 
-    venue: "DLI, BK2, GFLR(CBT)" 
+  {
+    id: 4,
+    course: "ZOO 101",
+    date: "Friday, March 27",
+    time: "9am - 3pm",
+    timestamp: "2026-03-27T09:00:00",
+    venue: "LT009 & LT018 & LT026"
   }
 ];
 
